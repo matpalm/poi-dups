59c4628a6ce42005d31609164ae0adf44d0dd437
mat <matthew.kelcey@gmail.com>
2010-08-21 16:57:23 +1000

    near dups, simple output, filter exact dups first

--- connected_components.rb
+++ connected_components.rb
@@ -5,30 +5,33 @@ require "rgl/adjacency"
 require "rgl/traversal"
 require "rgl/connected_components"
 
-incident_edge_sum = {}
-incident_edge_sum.default = 0
+class MaxResForPid
+  def initialize 
+    @max_res_for_pid = Hash.new(0)
+  end
+  def new_value res, pid
+    @max_res_for_pid[pid] = res if @max_res_for_pid[pid] < res
+  end
+  def max_res_for pids
+    pids.map{|v| @max_res_for_pid[v]}.max
+  end
+end
 
 g = RGL::AdjacencyGraph.new
+max_res_for_pid = MaxResForPid.new
 
 STDIN.each do |line|
   res,id1,id2 = line.split("\t")
   res,id1,id2 = res.to_f, id1.to_i, id2.to_i
-  incident_edge_sum[id1] += res
-  incident_edge_sum[id2] += res
+  id1,id2 = id2,id1 unless id1 < id2
   g.add_vertex id1
   g.add_vertex id2
   g.add_edge id1, id2
+  max_res_for_pid.new_value res, id1
+  max_res_for_pid.new_value res, id2
 end
 
 g.each_connected_component do |vertexs| 
-  max_sum = 0
-  max_vert = nil
-  vertexs.each do |vertex|
-    if incident_edge_sum[vertex] > max_sum
-      max_sum = incident_edge_sum[vertex]
-      max_vert = vertex
-    end
-  end
-  vertexs.delete max_vert
-  puts "#{max_vert} #{vertexs.join(' ')}"
+  max_res_in_group = max_res_for_pid.max_res_for vertexs
+  puts ([max_res_in_group] + vertexs).join("\t")
 end
--- csv_to_tsv.rb
+++ csv_to_tsv.rb
@@ -1,8 +1,9 @@
 #!/usr/bin/env ruby
 require 'csv'
 STDIN.each do |line|
-  next if line =~ /^id/
+  next if line =~ /^poi_id/
   line.gsub "\t",' '
-  id,name,place = CSV.parse_line(line)
-  puts [id,name,place].join("\t")
+  #puts CSV.parse_line(line).join("\t")
+  cols = CSV.parse_line(line)
+  puts [0,1,3].map{|i| cols[i]}.join("\t")
 end
